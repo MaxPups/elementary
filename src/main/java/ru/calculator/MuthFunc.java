622ca6c8704f98332405759f037acb98073c97c6
MaxPups <max_pups@bk.ru>
2023-09-23 16:19:56 +0300

    task from document 1.7 Результат работы метода

---
+++ src/main/java/ru/calculator/MuthFunc.java
@@ -0,0 +1,22 @@
+package ru.calculator;
+
+public class MathFunc {
+
+
+    public static int func1(int x) {
+        int y = x * x + 1;
+        return y;
+    }
+
+    public static int func2(int x) {
+        int y = 1 / x;
+        return y;
+    }
+
+    public static void main(String[] args) {
+        int res1 = MathFunc.func1(3);
+        int res2 = MathFunc.func1(5);
+        System.out.println(res1);
+        System.out.println(res2);
+    }
+}
