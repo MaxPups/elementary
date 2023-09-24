b47bb139dfa8e7212547daa32269c8531f930889
MaxPups <max_pups@bk.ru>
2023-09-24 16:30:32 +0300

    task from document 1.10 (#1) Ideal weight

---
+++ src/main/java/ru/calculator/Fit.java
@@ -0,0 +1,37 @@
+package ru.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        double rsl = (height - 100) * 1.15;
+        return rsl;
+    }
+
+    public static double womanWeight(short height) {
+        double rsl = (height - 110) * 1.15;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String  gender = "WOMAN";
+        short height = 150;
+        double woman = womanWeight(height);
+        System.out.println( gender +" "+ height +" is " + woman + " weight");
+
+        gender = "MAN";
+        height = 180;
+        double men = manWeight(height);
+        System.out.println(gender + " " + height + " is " + men + " weight");
+
+        gender = "MAN";
+        height = 200;
+        men = manWeight(height);
+        System.out.println(gender + " " + height + " is " + men + " weight");
+
+        gender = "WOMAN";
+        height = 175;
+        woman = womanWeight(height);
+        System.out.println(gender + " " + height + " is " + woman + " weight");
+    }
+
+}
