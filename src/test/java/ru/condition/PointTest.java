a64721fb2d5331a02c38222d56515e264c4f33e9
MaxPups <max_pups@bk.ru>
2023-09-29 00:39:29 +0300

    task from document before 2.5 (#3) test for beetwen point of coords

--- src/test/java/ru/condition/PointTest.java
+++ src/test/java/ru/condition/PointTest.java
@@ -18,8 +18,8 @@ class PointTest {
     }
 
     @Test
-    void when55to1010then7Dot0710678118654755() {
-        double expected = 7.0710678118654755;
+    void when55to1010then7Dot07() {
+        double expected = 7.07;
         int x1 = 5;
         int y1 = 5;
         int x2 = 10;
@@ -30,8 +30,8 @@ class PointTest {
 
 
     @Test
-    void when1525to203then22Dot561028345356956() {
-        double expected =22.561028345356956 ;
+    void when1525to203then22Dot56() {
+        double expected =22.56 ;
         int x1 = 15;
         int y1 = 25;
         int x2 = 20;
@@ -39,9 +39,10 @@ class PointTest {
         double out = Point.distance(x1, y1, x2, y2);
         assertThat(out).isEqualTo(expected, withPrecision(0.01));
     }
+
     @Test
-    void when333to9867then100Dot90094152187085() {
-        double expected =100.90094152187085;
+    void when333to9867then100Dot90() {
+        double expected =100.90;
         int x1 = 3;
         int y1 = 33;
         int x2 = 98;
@@ -51,4 +52,26 @@ class PointTest {
     }
 
 
+    @Test
+    void when5466to2267then32Dot01() {
+        double expected =32.01;
+        int x1 = 54;
+        int y1 = 66;
+        int x2 = 22;
+        int y2 = 67;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+
+    @Test
+    void when00to10098then140Dot01() {
+        double expected =140.01;
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 100;
+        int y2 = 98;
+        double out = Point.distance(x1, y1, x2, y2);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
 }
