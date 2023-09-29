24a7206d06c2df1572bc37f210d3ac4f3dade79c
MaxPups <max_pups@bk.ru>
2023-09-29 10:26:50 +0300

    task from document before 2.6 (#1) test for sqArea

---
+++ src/test/java/ru/condition/SqAreaTest.java
@@ -0,0 +1,45 @@
+package ru.condition;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SqAreaTest {
+
+    @Test
+    void whenP6K2Square2() {
+        int expected = 2;
+        int p = 6;
+        double k = 2;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP8K1Square4() {
+        int expected = 4;
+        int p = 8;
+        double k = 1;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+    @Test
+    void whenP16K3Square12() {
+        int expected = 12;
+        int p = 16;
+        double k = 3;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+
+
+    @Test
+    void whenP30K4Square36() {
+        int expected = 36;
+        int p = 30;
+        double k = 4;
+        double out = SqArea.square(p, k);
+        assertThat(out).isEqualTo(expected, withPrecision(0.01));
+    }
+}
