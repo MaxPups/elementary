845c8becf6022408b32ecf997eac74334b69e478
MaxPups <max_pups@bk.ru>
2023-09-30 22:38:19 +0300

    task from document before 3.3 (#1) operator compare for String

---
+++ src/main/java/ru/condition/StringEq.java
@@ -0,0 +1,16 @@
+package ru.condition;
+
+public class StringEq {
+
+    public static void main(String[] args) {
+        String first = new String("one");
+        String one = new String("one");
+        if (first.equals(one)) {
+            System.out.println("first equals one");
+        } else {
+            System.out.println("first does not equal one");
+        }
+
+    }
+
+}
