2db92df5755e2494cf1355973aaa2a929f4d883e
MaxPups <max_pups@bk.ru>
2023-10-04 12:47:33 +0300

    task from document before 3.16 (#2) debug

--- src/main/java/ru/condition/ThreeMax.java
+++ src/main/java/ru/condition/ThreeMax.java
@@ -3,16 +3,21 @@ package ru.condition;
 public class ThreeMax {
 
     public static int max(int first, int second, int third) {
-        int result = first;
+        int result = third;
         if (first > second && first > third) {
             result = first;
         }
         if (second > first && second > third) {
             result = second;
         }
+        if (first == third || second == third) {
+            result = third;
+        }
+        if (first == second) {
+            result = first;
+        }
         return result;
     }
-
 
     public static void main(String[] args) {
         int res = max(10, 9, 8);
