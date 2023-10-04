64a0a801c5291effd1f8be1a72b5903a9d77100b
MaxPups <max_pups@bk.ru>
2023-10-04 23:21:55 +0300

    task from document before 4.5 (#1) factorial

---
+++ src/main/java/ru/loop/Factorial.java
@@ -0,0 +1,11 @@
+package ru.loop;
+
+public class Factorial {
+    public static int calc(int n) {
+        int res = 1;
+        for (int i = 1; i <= n; i++) {
+            res *= i;
+        }
+        return res;
+    }
+}
