82c882c24ca4509bed5595b518e1d33959af0b48
MaxPups <max_pups@bk.ru>
2023-10-08 20:58:33 +0300

    task from document before 4.10 (#2) ipoteka

--- src/main/java/ru/loop/Mortgage.java
+++ src/main/java/ru/loop/Mortgage.java
@@ -4,15 +4,11 @@ public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
         while (amount > 0) {
-            amount += amount * (percent/100);
-            amount = amount - salary;
+            amount += amount * (percent / 100);
+            amount -= salary;
             year++;
         }
         return year;
     }
 
-    public static void main(String[] args) {
-        int res = year(200.0, 60, 20.0);
-        System.out.println(res);
-    }
 }
