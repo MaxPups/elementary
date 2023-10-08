7726b8195641adad09b08708d52d379a391161dd
MaxPups <max_pups@bk.ru>
2023-10-08 19:28:15 +0300

    task from document before 4.9 (#1) prime number #2

---
+++ src/main/java/ru/loop/PrimeNumber.java
@@ -0,0 +1,19 @@
+package ru.loop;
+
+public class PrimeNumber {
+    public static int calc(int finish) {
+        int count = 0;
+        for (int i = 2; i < finish; i++) {
+            if (CheckPrimeNumber.check(i)) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int res = calc(20);
+        System.out.println(res);
+    }
+
+}
