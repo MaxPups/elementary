8b44524dc9c54f2a87cc7ff8caf4d30a99c70e2b
MaxPups <max_pups@bk.ru>
2023-10-25 16:44:36 +0300

    task from document before 5.5 (#1) array and loop for-each

--- src/main/java/ru/array/ArrayLoop.java
+++ src/main/java/ru/array/ArrayLoop.java
@@ -4,10 +4,10 @@ public class ArrayLoop {
     public static void main(String[] args) {
         int[] numbers = new int[5];
         for (int i = 0; i < numbers.length; i++) {
-            numbers[i]= i *2 +3;
+            numbers[i] = i * 2 + 3;
         }
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i]);
+        for (int i : numbers) {
+            System.out.println(i);
         }
     }
 }
