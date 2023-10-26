3bf81e32f6b117b4cc5d547f72db200666e0996e
MaxPups <max_pups@bk.ru>
2023-10-26 18:11:26 +0300

    task from document before 5.13 (#2) is full array by true or false

--- src/main/java/ru/array/Check.java
+++ src/main/java/ru/array/Check.java
@@ -3,16 +3,12 @@ package ru.array;
 public class Check {
     public static boolean mono(boolean[] data) {
         boolean result = true;
-        boolean val = false;
-        val = data[0];
-        for (boolean value : data
-        ) {
-            if (val != value) {
-                result = false;
+        for (int i = 0; i < data.length; i++) {
+            if (data[0] != data[i]) {
+                result = !result;
                 break;
             }
         }
-
 
         return result;
     }
