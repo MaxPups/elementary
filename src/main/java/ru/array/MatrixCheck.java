1920cc27f4ed463c1448810aed0b07af71696b62
MaxPups <max_pups@bk.ru>
2023-10-30 19:14:47 +0300

    task from document before 5.30 (#1) winstrike in sokoban

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -3,12 +3,10 @@ package ru.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 1; j < board[i].length; j++) {
-                if (board[i][j] != board[i][0]) {
-                    result = false;
-                    break;
-                }
+        for (int i = 1; i < board[row].length; i++) {
+            if (board[row][i] != board[row][0]) {
+                result = false;
+                break;
             }
         }
         return result;
@@ -21,13 +19,27 @@ public class MatrixCheck {
                 result = false;
                 break;
             }
-        } return result;
+        }
+        return result;
     }
 
     public static char[] extractDiagonal(char[][] board) {
         char[] rsl = new char[board.length];
         for (int i = 0; i < board.length; i++) {
             rsl[i] = board[i][i];
-        } return rsl;
+        }
+        return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
+
 }
--- src/test/java/ru/array/MatrixCheckTest.java
+++ src/test/java/ru/array/MatrixCheckTest.java
@@ -23,9 +23,9 @@ public class MatrixCheckTest {
     @Test
     public void whenHasNotMonoHorizontal() {
         char[][] input = {{
-                'E', ' ', ' '
+                'X', ' ', ' '
         }, {
-                'X', 'X', 'X'
+                '-', 'X', 'X'
         }, {
                 ' ', '-', ' '
         },};
@@ -92,7 +92,6 @@ public class MatrixCheckTest {
     }
 
     @Test
-
     public void whenDiagonalMix() {
         char[][] input = {{
                 'X', ' ', ' '
@@ -104,5 +103,39 @@ public class MatrixCheckTest {
         char[] result = MatrixCheck.extractDiagonal(input);
         char[] expected = {'X', 'Y', 'Z'};
         assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    public void whenDataMonoByTrueThenTrue() {
+        char[][] input = {{' ', ' ', 'X', ' ', ' '}, {' ', ' ', 'X', ' ', ' '}, {' ', ' ', 'X', ' ', ' '}, {' ', ' ', 'X', ' ', ' '}, {' ', ' ', 'X', ' ', ' '},};
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenDataNotMonoByTrueThenFalse() {
+        char[][] input = {{' ', ' ', 'X', ' ', ' '}, {' ', ' ', 'X', ' ', ' '}, {' ', 'X', ' ', ' ', ' '}, {' ', ' ', 'X', ' ', ' '}, {' ', ' ', 'X', ' ', ' '},};
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result).isFalse();
+    }
+
+    @Test
+    public void whenDataHMonoByTrueThenTrue() {
+        char[][] input = {
+                {' ', ' ', ' ', ' ', ' '},
+                {' ', ' ', ' ', ' ', ' '},
+                {'X', 'X', 'X', 'X', 'X'},
+                {' ', ' ', 'X', ' ', ' '},
+                {' ', ' ', 'X', ' ', ' '}
+        };
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    public void whenDataDiagMonoIsTrueThenFalse() {
+        char[][] input = {{'X', ' ', ' ', ' ', ' '}, {' ', 'X', ' ', ' ', ' '}, {' ', ' ', 'X', ' ', ' '}, {' ', ' ', ' ', 'X', ' '}, {' ', ' ', ' ', ' ', 'X'},};
+        boolean result = MatrixCheck.isWin(input);
+        assertThat(result).isFalse();
     }
 }
