d38f4f6f35530c99c1caa53610b67f26faa9b0ae
MaxPups <max_pups@bk.ru>
2023-10-31 12:41:33 +0300

    task from document before: 5.30 (№2) winstrike in sokoban

--- src/main/java/ru/array/MatrixCheck.java
+++ src/main/java/ru/array/MatrixCheck.java
@@ -37,7 +37,11 @@ public class MatrixCheck {
     public static boolean isWin(char[][] board) {
         boolean result = false;
         for (int i = 0; i < board.length; i++) {
+            if (board[i][i] != 'X') {
+                continue;
+            }
             if (monoHorizontal(board, i) || monoVertical(board, i)) {
+                System.out.println(i);
                 result = true;
                 break;
             }
